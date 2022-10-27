675a8e75d8ea3ac18c654705e7f0d8314a4a667a
Eyal Zusiman <eyal.zusiman@gmail.com>
2022-10-27 19:22:59 +0300

    Extended test block horizontally and fixed diagonal bounces

--- Scripts/Character/States/BaseState.cs
+++ Scripts/Character/States/BaseState.cs
@@ -9,27 +9,19 @@ public class BaseState : State
 
     public override Action<Character> Loop(float delta) => c =>
     {
+        //apply movement
+        c.MoveAndSlide(c.CurrentVelocity, Vector2.Zero);
+
         //detect bounce
         if(c.DashBounceActive && c.IsOnWall())
         {
-            //prevent glitchy multibounce
-            c.DashBounceActive = false;
-
-            //iterate collisions
-            for(int i = 0; i < c.GetSlideCount(); ++i)
-            {
-                //get collision
-                var col = c.GetSlideCollision(i);
-                //bounce along collision normal
-                c.CurrentVelocity = c.CurrentVelocity.Bounce(col.Normal);
-            }
-
+            //get first collision
+            var col = c.GetSlideCollision(0);
+            //bounce along collision normal
+            c.CurrentVelocity = c.CurrentVelocity.Bounce(col.Normal);
             //apply bounce deacceleration
             c.CurrentVelocity = c.CurrentVelocity.MoveToward(Vector2.Zero, c.DashBounceDeacceleration);
         }
-
-        //apply movement
-        c.MoveAndSlide(c.CurrentVelocity, Vector2.Zero);
 
         //update inputs and movement
         c.SetInputs();
