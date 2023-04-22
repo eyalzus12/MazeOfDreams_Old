f2eccc10e865ca6e29c54e2cad4eaf7976f09301
Eyal Zusiman <eyal.zusiman@gmail.com>
2023-04-22 21:28:08 +0300

    added locking to the inventory slot container

--- Objects/UI/InventorySlot/InventorySlotContainer.gd
+++ Objects/UI/InventorySlot/InventorySlotContainer.gd
@@ -3,6 +3,8 @@ class_name InventorySlotContainer
 @export var allow_category: Array[String] = []
 @export var block_category: Array[String] = []
 
+@export var is_locked: bool = false
+
 @export var inventory: Inventory
 @export var i: int
 @export var j: int
@@ -12,6 +14,7 @@ class_name InventorySlotContainer
 func _ready() -> void:
 	slot.allow_category = allow_category
 	slot.block_category = block_category
+	slot.is_locked = is_locked
 	slot.inventory = inventory
 	slot.i = i
 	slot.j = j
